aa642bc0a2106a8e23a7f8d8da45d0e50350e8e1
Imaraika <inange2013@gmail.com>
2019-08-29 19:40:23 +0200

    changes have been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchFormComponent } from './search-form/search-form.component';
+import {RepositoryComponent} from './repository/repository.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'users', component: SearchFormComponent},
+  {path: 'repository', component: RepositoryComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
+
 })
 export class AppRoutingModule { }
