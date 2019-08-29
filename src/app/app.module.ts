d45ea12f511c062e9ffcca90d3e01a3985b1eacc
Imaraika <inange2013@gmail.com>
2019-08-29 19:54:07 +0200

    changes have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,9 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchRequestService} from '../search-request.service';
+import {Repository} from '../repository';
+import {User} from '../user';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  public searchMe = 'Imaraika';
+  public githubUser: string;
 
-  ngOnInit() {
+  users: User ;
+  repository: Repository;
+  public searchRepo: string;
+  public resultCount = 12;
+
+
+  findUser(username) {
+      this.githubUser = '';
+      this.searchMe  = username;
+      this.ngOnInit();
   }
 
+
+constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+}
+
+
+  searchRepos() {
+      this.searchRepo = '';
+      this.resultCount = 10;
+
+  }
 }
